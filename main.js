29b04105a33242d15f78f29adabfb389c1716f20
Flo Leeroy <florian.leeroy@hotmail.com>
2019-11-15 15:46:33 +0100

    ajout des tags pour pata et tout

--- main.js
+++ main.js
@@ -26,38 +26,46 @@ Theses functions were not used anymore so i removed them from the code
 bot.on('ready', function (evt) {
     logger.info("Running")
 })
-bot.on('message', function (message) {
-    if(!message.content.includes(" ") && message.mentions.users.first(undefined) === message.mentions.users.last(undefined)){
-        //on a un seul tag
-        message.reply("TAG")
-        if(message.mentions.users.firstKey(undefined) === "112632359207108608"){
-            message.channel.send("Le plus beau !")
-        }
-    }
-    
+bot.on('message', function (message) {    
     var texte = message.content.toLowerCase();
+    //comparaisons
     if(texte === "ok"){
         message.channel.send("sur glace!")
-    }
-    if((texte.includes("slt") || texte.includes("salut") || texte.includes("bonjour")) && message.author !== bot.user){
-        message.channel.send("slt mon bro :wave:")
-    }
-    if(texte.includes("tg") && message.author !== bot.user){
-        message.reply("toi tg!")
-    }
-    if(texte === "gg"){
+    }else if(texte === "gg"){
         message.channel.send("EZ")
-    }
-    if(texte.includes("@everyone")){
-        message.reply("nique ta mère avec ton tag everyone !")
-    }
-    if(texte.includes("@here")){
-        message.reply("nique ta mère avec ton tag here !")
-    }
-    if(texte === "mdr" || texte === "lol"){
+    }else if(texte === "mdr" || texte === "lol"){
         message.channel.send("OH MOI AUSSI CA M'A TUÉ JPP ! XD")
-    }
-    if(texte === "quoi" || texte === "quoi?" || texte === "quoi ?"){
+    }else if(texte === "quoi" || texte === "quoi?" || texte === "quoi ?"){
         message.channel.send("T'es de la police ?")
+    }else{
+        //includes
+        if((texte.includes("slt") || texte.includes("salut") || texte.includes("bonjour")) && message.author !== bot.user){
+            message.channel.send("slt mon bro :wave:")
+        }
+        if(texte.includes("tg") && message.author !== bot.user){
+            message.reply("toi tg!")
+        }
+        if(texte.includes("@everyone")){
+            message.reply("nique ta mère avec ton tag everyone !")
+        }
+        if(texte.includes("@here")){
+            message.reply("nique ta mère avec ton tag here !")
+        }
+        
+        //par rapport aux tags
+        if(!message.content.includes(" ") && message.mentions.users.first(undefined) === message.mentions.users.last(undefined)){
+            //on a un seul tag
+            if(message.mentions.users.firstKey(undefined) === "112632359207108608"){ //Flo
+                message.channel.send("Le plus beau !")
+            }else if(message.mentions.users.firstKey(undefined) === "216919708560130048"){ //Coco
+                message.channel.send("Le plus fort !")
+            }else if(message.mentions.users.firstKey(undefined) === "230698146630598656"){ //Pata
+                message.channel.send("Le plus classe !")
+            }else if(message.mentions.users.firstKey(undefined) === "311176545383219202"){ //Didou
+                message.channel.send("La plus bonne ❤️")
+            }else if(message.mentions.users.firstKey(undefined) === "302898536356511745"){ //o4
+                message.channel.send("Le plus déco ...")
+            }
+        }
     }
 })
