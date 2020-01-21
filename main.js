05b8e40c45be914488983a4801c5891bbf281e4b
Floliroy <florian.leeroy@hotmail.com>
2020-01-21 23:56:01 +0100

    Update main.js

--- main.js
+++ main.js
@@ -280,14 +280,15 @@ bot.on('message', function (message) {
     }
     doc.useServiceAccountAuth(creds, function(err) {
         console.log("Je me suis authentifié")
-        doc.getCells(1, options, function(err, cells) {
-            console.log("Je lis atm, taille : " + cells.length)
-            jour = cells[0].value()
-            matiere = cells[1].value()
-            description = cells[2].value()
-            imageURL = cells[3].value()
-            lien = cells[4].value()
-        })
+    })
+
+    doc.getCells(1, options, function(err, cells) {
+        console.log("Je lis atm, taille : " + cells.length)
+        jour = cells[0].value()
+        matiere = cells[1].value()
+        description = cells[2].value()
+        imageURL = cells[3].value()
+        lien = cells[4].value()
     })
 
     if(message.content === "test" && message.channel.guild.id === serversID.cira){
