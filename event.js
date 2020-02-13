8fbd8ff818902099da207b93bb613b1694db44d0
Flo Leeroy <florian.leeroy@hotmail.com>
2020-02-13 19:08:39 +0100

    modif

--- event.js
+++ event.js
@@ -6,31 +6,27 @@ const channelsID={
 }
 
 Date.prototype.addDays = function(days) {
-    var date = new Date(this.valueOf());
+    let date = new Date(this.valueOf());
     date.setDate(date.getDate() + days);
     return date;
 }
 
 async function envoieJours(chan){
     const jours = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
-
-    //let ddSave
-    
     let cpt = 1
 
-    for await(let jour of jours){
-        
-        let date = new Date('December 28, 1995 03:24:00')
+    jours.forEach(function(elem){    
+        let date = new Date('December 28, 1995 03:24:00') 
         date.addDays(cpt)
         cpt++
         let dd = date.getDate()
         let mm = date.getMonth() + 1
-        /*if(dd < ddSave){
-            mm++
-        }
-        ddSave = dd*/
-        console.log(`${jour} ${String(dd).padStart(2, '0')}/${String(mm).padStart(2, '0')}`)
-        chan.send({embed: {title : `${jour} ${String(dd).padStart(2, '0')}/${String(mm).padStart(2, '0')}`}})
+        elem = `${elem} ${String(dd).padStart(2, '0')}/${String(mm).padStart(2, '0')}`
+        console.log(`${elem}`)
+    })
+
+    for await(let jour of jours){
+        chan.send({embed: {title : `${jour}`}})
         .then(msg => {msg.react("✅")
             .then(() => msg.react("❌"))
             .then(() => msg.react("➖"))
--- main.js
+++ main.js
@@ -27,7 +27,7 @@ bot.on('message', function (message) {
     Cira.ciraMessage(message, bot)
 })
 
-cron.schedule("02 19 * * Thursday", function() {
+cron.schedule("10 19 * * Thursday", function() {
     console.log("-- Cron Started --")
     Event.eventMessage(bot)
 }, {
