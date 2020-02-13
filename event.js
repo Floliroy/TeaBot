3fa4dcbc2ae7a5676f869245cce9b79f3eaf7080
Flo Leeroy <florian.leeroy@hotmail.com>
2020-02-13 20:49:37 +0100

    Update event.js

--- event.js
+++ event.js
@@ -80,7 +80,7 @@ module.exports = class Event{
         const chan = bot.channels.get(channelsID.team_lol)
         const msg = await getDayliMessage(bot)
 
-        if(msg != null && msg.reactions.find(val => val.name === "❌").count > 1){
+        if(msg != null && msg.reactions.get("❌").users.size > 1){
             console.log("NON")
         }else if(msg != null){
             console.log("OUI")
